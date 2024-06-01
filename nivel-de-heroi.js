8657227f9d2344d6026a16e148e06724f5a5ff58
wladson-nobre <wladson.nobre@gmail.com>
2024-06-01 09:44:43 -0300

    desafio-nivel-de-heroi

---
+++ nivel-de-heroi.js
@@ -0,0 +1,21 @@
+let nome = "Wladson";
+let xp = 10500;
+
+if (xp < 1000) {
+    console.log("O Herói de nome " + nome + " está no nível de Ferro!");
+} else if (xp > 1000 && xp <= 2000) {
+    console.log("O Herói de nome " + nome + " está no nível de Bronze!");
+} else if (xp > 2000 && xp <= 5000) {
+    console.log("O Herói de nome " + nome + " está no nível de Prata!");
+} else if (xp > 5000 && xp <= 7000) {
+    console.log("O Herói de nome " + nome + " está no nível de Ouro!");
+} else if (xp > 7000 && xp <= 8000) {
+    console.log("O Herói de nome " + nome + " está no nível de Platina!");
+} else if (xp > 8000 && xp <= 9000) {
+    console.log("O Herói de nome " + nome + " está no nível de Ascendente!");
+} else if (xp > 9000 && xp <= 10000) {
+    console.log("O Herói de nome " + nome + " está no nível de Imortal!");
+} else {
+    console.log("O Herói de nome " + nome + " está no nível de Radiante!");
+}
+       
